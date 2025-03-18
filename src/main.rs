6d605e18122f577fb198af31bd72838b9e0adb2c
IkuoShige <shigeikuo@gmail.com>
2025-03-19 05:52:36 +0900

    Update rclrs API usage to align with latest changes(ros2_rust#429)

--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,58 @@ use sensor_msgs::msg::LaserScan;
 use nav_msgs::msg::OccupancyGrid;
 use visualization_msgs::msg::MarkerArray;
 use std::ops::Deref;
+use rclrs::{
+    Node, 
+    Subscription, 
+    Publisher, 
+    Executor, 
+    RclrsError, 
+    Context, 
+    SpinOptions, 
+    CreateBasicExecutor, 
+    RclrsErrorFilter
+};
 
 struct FlowEstimatorNode {
-    node: Arc<rclrs::Node>,
-    _sub_scan: Arc<rclrs::Subscription<LaserScan>>,
+    node: Arc<Node>,
+    _sub_scan: Mutex<Option<Arc<Subscription<LaserScan>>>>,
     data: Arc<Mutex<Option<LaserScan>>>,
-    scan_map: Arc<rclrs::Publisher<OccupancyGrid>>,
-    static_obstacle_map: Arc<rclrs::Publisher<OccupancyGrid>>,
-    obstacle_motion: Arc<rclrs::Publisher<MarkerArray>>,
+    scan_map: Arc<Publisher<OccupancyGrid>>,
+    static_obstacle_map: Arc<Publisher<OccupancyGrid>>,
+    obstacle_motion: Arc<Publisher<MarkerArray>>,
 }
 
 impl FlowEstimatorNode {
-    fn new(context: &rclrs::Context) -> Result<Self, rclrs::RclrsError> {
-        let node = rclrs::Node::new(context, "flow_estimator")?;
+    fn new(executor: &Executor) -> Result<Arc<Self>, RclrsError> {
+        let node = executor.create_node("flow_estimator")?;
+        
+        let scan_map = node.create_publisher::<OccupancyGrid>("scan_map")?;
+        let static_obstacle_map = node.create_publisher::<OccupancyGrid>("static_obstacle_map")?;
+        let obstacle_motion = node.create_publisher::<MarkerArray>("estimation_array")?;
+        
         let data = Arc::new(Mutex::new(None));
-        let data_cb = Arc::clone(&data);
-        let _sub_scan = node.create_subscription(
-            "scan",
-            rclrs::QOS_PROFILE_DEFAULT,
-            move |msg: LaserScan| { *data_cb.lock().unwrap() = Some(msg); },
-        )?;
-
-        let scan_map = node.create_publisher("scan_map", rclrs::QOS_PROFILE_DEFAULT)?;
-        let static_obstacle_map = node.create_publisher("static_obstacle_map", rclrs::QOS_PROFILE_DEFAULT)?;
-        let obstacle_motion = node.create_publisher("estimation_array", rclrs::QOS_PROFILE_DEFAULT)?;
-        Ok(Self {
+        
+        let flow_estimator_node = Arc::new(FlowEstimatorNode {
             node,
-            _sub_scan,
+            _sub_scan: None.into(),
             data,
             scan_map,
             static_obstacle_map,
             obstacle_motion,
-        })
+        });
+        
+        let data_cb = Arc::clone(&flow_estimator_node.data);
+        let subscription = flow_estimator_node.node
+            .create_subscription::<LaserScan, _>("scan", move |msg: LaserScan| { 
+                *data_cb.lock().unwrap() = Some(msg); 
+            })?;
+        
+        *flow_estimator_node._sub_scan.lock().unwrap() = Some(subscription);
+        
+        Ok(flow_estimator_node)
     }
-
-    fn publish_scan_map(&self, buffer: &mut Vec<OccupancyGrid>) -> Result<(), rclrs::RclrsError> {
+    
+    fn publish_scan_map(&self, buffer: &mut Vec<OccupancyGrid>) -> Result<(), RclrsError> {
         let scan = match self.data.lock().unwrap().deref() {
             Some(s) => s.clone(),
             None => {
@@ -55,52 +72,53 @@ impl FlowEstimatorNode {
                 return Ok(());
             },
         };
-
+        
         if let Some(last_map) = buffer.last() {
             if last_map.info.map_load_time == scan.header.stamp {
                 return Ok(());
             }
         }
-
+        
         let map = scan_map::generate(120, 120, 0.1, &scan);
         buffer.push(map.clone());
-        self.scan_map.publish(map)?;
-
+        self.scan_map.publish(&map)?;
+        
         if buffer.len() > 100 {
             buffer.remove(0);
         }
-
+        
         Ok(())
     }
-
-    fn publish_static_obstacle_map(&self, map: &OccupancyGrid) -> Result<(), rclrs::RclrsError> {
+    
+    fn publish_static_obstacle_map(&self, map: &OccupancyGrid) -> Result<(), RclrsError> {
         self.static_obstacle_map.publish(map)?;
         Ok(())
     }
-
-    fn publish_obstacle_motion(&self, map: &MarkerArray) -> Result<(), rclrs::RclrsError> {
+    
+    fn publish_obstacle_motion(&self, map: &MarkerArray) -> Result<(), RclrsError> {
         self.obstacle_motion.publish(map)?;
         Ok(())
     }
 }
 
-fn main() -> Result<(), rclrs::RclrsError> {
-    let context = rclrs::Context::new(std::env::args())?;
-    let republisher = Arc::new(FlowEstimatorNode::new(&context)?);
+fn main() -> Result<(), RclrsError> {
+    let mut executor = Context::default_from_env()?.create_basic_executor();
+    let republisher = FlowEstimatorNode::new(&executor)?;
+    
     let republisher_other_thread = Arc::clone(&republisher);
-
     let mut map_buffer = vec![];
-
-    std::thread::spawn(move || -> Result<(), rclrs::RclrsError> {
+    
+    std::thread::spawn(move || -> Result<(), RclrsError> {
         let mut estimator = Estimator::new();
-
         loop {
             use std::time::Duration;
             std::thread::sleep(Duration::from_millis(200));
+            
             republisher_other_thread.publish_scan_map(&mut map_buffer)?;
-
+            
             if let Some(static_map) = static_map::generate(&mut map_buffer) {
                 republisher_other_thread.publish_static_obstacle_map(&static_map)?;
+                
                 match estimator.generate(&mut map_buffer, &static_map) {
                     Ok(Some(estimation)) => 
                         republisher_other_thread.publish_obstacle_motion(&estimation)?,
@@ -110,6 +128,6 @@ fn main() -> Result<(), rclrs::RclrsError> {
             }
         }
     });
-
-    rclrs::spin(Arc::clone(&republisher.node))
+    
+    executor.spin(SpinOptions::default()).first_error()
 }
