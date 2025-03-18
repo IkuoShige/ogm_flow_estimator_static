b0b92bd6d9d4d6cf6697ff6308db12cfd8a4fad1
IkuoShige <shigeikuo@gmail.com>
2025-03-19 06:05:19 +0900

    Update test for launch

--- launch/run_with_bag.bash
+++ launch/run_with_bag.bash
@@ -3,4 +3,5 @@
 dir=$(dirname $0)/..
 
 ( sleep 5 && ros2 bag play $dir/bag/rosbag2_2025_01_22-13_29_26 ) &
-cargo run --release
+
+ros2 launch ogm_flow_estimator_static ogm_flow_estimator_static.launch.py
--- test/test.bash
+++ test/test.bash
@@ -9,9 +9,8 @@ colcon build
 source $dir/.bashrc
 
 cd $dir/ros2_ws/src/ogm_flow_estimator_static
-cargo build --release
 
 ( sleep 5 && ros2 bag play ./bag/rosbag2_2025_01_22-13_29_26 ) &
-timeout 30 cargo run --release |& tee - log.txt
+timeout 30 ros2 launch ogm_flow_estimator_static ogm_flow_estimator_static.launch.py use_rviz:=false |& tee log.txt
 
 grep -q END log.txt
