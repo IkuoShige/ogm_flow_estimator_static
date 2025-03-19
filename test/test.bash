ae779c4ab3d853d1ffb0de4072135bf4b5e3245e
IkuoShige <shigeikuo@gmail.com>
2025-03-19 15:55:00 +0900

    Update ros2_rust packages

--- test/test.bash
+++ test/test.bash
@@ -4,6 +4,9 @@ dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 source $dir/.bashrc
+cd $dir/ros2_ws/src/ros2_rust
+git pull origin main
+git checkout becc938e855587d75237cf82b9c312ebc49136ae
 cd $dir/ros2_ws
 rm -rf $dir/ros2_ws/src/ros2 $dir/ros2_ws/src/ros2-rust
 vcs import src < src/ros2_rust/ros2_rust_humble.repos
